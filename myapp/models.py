05af855dd4a97c235d91f34ad0c3cb5799414844
vyu <vyuvyuvyu@naver.com>
2025-03-03 22:11:15 +0900

    feat: models - Link 모델 추가 (sqlite)

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+    # get the name field instead of getting objct of this(self)
+    def __str__(self):
+        return self.name
+
+    adress =    models.CharField(max_length=1000,null=True,blank=True)
+    name =      models.CharField(max_length=1000,null=True,blank=True)
