8a17bbc4d1d8f416b8d2986d298e213c6238d966
vyu <vyuvyuvyu@naver.com>
2025-03-03 22:27:36 +0900

    fix: result 화면 주석 지워주기 (html comment -> error)

--- myapp/views.py
+++ myapp/views.py
@@ -1,16 +1,22 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from .models import Link
 
 def scrape(request):
     page = requests.get('https://www.facebook.com')
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    link_address = []
+    # link_address = []
+    # for link in soup.find_all('a'):
+    #     link_address.append(link.get('href'))
 
     for link in soup.find_all('a'):
-        link_address.append(link.get('href'))
+        link_address = link.get('href')
+        link_text = link.string
+        Link.objects.create(address=link_address, name=link_text)
 
-    print(link_address)
+    data = Link.objects.all()
 
-    return render(request, 'myapp/result.html', {'link_adress': link_address})
+    # return render(request, 'myapp/result.html', {'link_adress': link_address})
+    return render(request, 'myapp/result.html', {'data': data})
