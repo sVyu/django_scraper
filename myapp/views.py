818a3a4b7232ed43757e55776d062e0b97ea2ab3
vyu <vyuvyuvyu@naver.com>
2025-03-03 22:00:27 +0900

    feat: 루트 화면 result 추가 # myapp/views.py에 scrape 함수 정의

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+def scrape(request):
+    page = requests.get('https://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    print(link_address)
+
+    return render(request, 'myapp/result.html', {'link_adress': link_address})
