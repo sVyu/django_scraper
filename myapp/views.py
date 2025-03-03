726fd68c9888723f1e4ee7a84e0f7f9d36458067
vyu <vyuvyuvyu@naver.com>
2025-03-03 23:08:07 +0900

    feat: input & submit POST 요청에 따라 web scraping 가능하게 처리 및 delete 기능도 추가

--- myapp/views.py
+++ myapp/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpResponseRedirect
 from .models import Link
 
 def scrape(request):
-    page = requests.get('https://www.facebook.com')
-    soup = BeautifulSoup(page.text, 'html.parser')
+    if request.method == "POST":
+        site = request.POST.get('site', '')
 
-    # link_address = []
-    # for link in soup.find_all('a'):
-    #     link_address.append(link.get('href'))
+        # page = requests.get('https://www.facebook.com')
+        page = requests.get(site)
+        soup = BeautifulSoup(page.text, 'html.parser')
 
-    for link in soup.find_all('a'):
-        link_address = link.get('href')
-        link_text = link.string
-        Link.objects.create(address=link_address, name=link_text)
+        # link_address = []
+        # for link in soup.find_all('a'):
+        #     link_address.append(link.get('href'))
 
-    data = Link.objects.all()
+        for link in soup.find_all('a'):
+            link_address = link.get('href')
+            link_text = link.string
+            Link.objects.create(address=link_address, name=link_text)
+        return HttpResponseRedirect('/')
+    else:
+        data = Link.objects.all()
 
     # return render(request, 'myapp/result.html', {'link_adress': link_address})
     return render(request, 'myapp/result.html', {'data': data})
+
+def clear(requests):
+    Link.objects.all().delete()
+    return render(requests, 'myapp/result.html')
