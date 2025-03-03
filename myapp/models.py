defedf27a2dae1efe6c5232b1bcd85c5401a7b7d
vyu <vyuvyuvyu@naver.com>
2025-03-03 22:18:11 +0900

    fix: Link model (field) adress -> address

--- myapp/models.py
+++ myapp/models.py
@@ -5,5 +5,5 @@ class Link(models.Model):
     def __str__(self):
         return self.name
 
-    adress =    models.CharField(max_length=1000,null=True,blank=True)
+    address =    models.CharField(max_length=1000,null=True,blank=True)
     name =      models.CharField(max_length=1000,null=True,blank=True)
